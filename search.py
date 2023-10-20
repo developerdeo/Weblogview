70e136b73f54eda5467576c55cb3d66eea44ae35
developerdeo <136994629+developerdeo@users.noreply.github.com>
2023-10-20 15:53:27 +0300

    final
    final output complete

--- search.py
+++ search.py
@@ -6,60 +6,60 @@ file = open(xl, 'r')
 content = file.read()
         # check if string present or not
 if '1=1' in content:
-    print('SQL INJECTION ATTACK DETECTED\n 1=1 found in log')
+    print('SQL INJECTION ATTACK DETECTED\n 1=1 found ')
 elif 'OR' in content:
-    print('SQL INJECTION ATTACK DETECTED\n OR found in log')
+    print('SQL INJECTION ATTACK DETECTED\n OR found ')
 elif 'INSERT' in content:
-    print('SQL INJECTION ATTACK DETECTED\n INSERT found in log')
+    print('SQL INJECTION ATTACK DETECTED\n INSERT found ')
 elif 'SELECT' in content:
-    print('SQL INJECTION ATTACK DETECTED\n SELECT found in log')
+    print('SQL INJECTION ATTACK DETECTED\n SELECT found ')
 elif 'WHERE' in content:
-    print('SQL INJECTION ATTACK DETECTED\n WHERE found in log')
+    print('SQL INJECTION ATTACK DETECTED\n WHERE found ')
 elif 'union' in content:
-    print('SQL INJECTION ATTACK DETECTED\n union found in log')
+    print('SQL INJECTION ATTACK DETECTED\n union found ')
 elif 'alert' in content:
-    print('CROSS SITE SCRIPTING ATTACK DETECTED\n alert found in log')
+    print('CROSS SITE SCRIPTING ATTACK DETECTED\n alert found ')
 elif 'script' in content:
-    print('CROSS SITE SCRIPTING ATTACK DETECTED\n script found in log')
+    print('CROSS SITE SCRIPTING ATTACK DETECTED\n script found ')
 elif 'ALERT' in content:
-    print('CROSS SITE SCRIPTING ATTACK DETECTED\n ALERT found in log')
+    print('CROSS SITE SCRIPTING ATTACK DETECTED\n ALERT found ')
 elif 'SCRIPT' in content:
-    print('CROSS SITE SCRIPTING ATTACK DETECTED\n SCRIPT found in log')
+    print('CROSS SITE SCRIPTING ATTACK DETECTED\n SCRIPT found ')
 elif 'ECHO' in content:
-    print('COMMAND INJECTION ATTACK DETECTED\n ECHO found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n ECHO found ')
 elif 'DIR' in content:
-    print('COMMAND INJECTION ATTACK DETECTED\n DIR found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n DIR found ')
 elif 'LS' in content:
-    print('COMMAND INJECTION ATTACK ATTACK DETECTED\n LS found in log')
+    print('COMMAND INJECTION ATTACK ATTACK DETECTED\n LS found ')
 elif 'CP' in content:
-    print('COMMAND INJECTION ATTACK DETECTED\n CP found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n CP found ')
 elif 'CAT' in content:
-    print('COMMAND INJECTION ATTACK DETECTED\n CAT found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n CAT found ')
 elif 'TYPE' in content:
-    print('COMMAND INJECTION ATTACK DETECTED\n TYPE found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n TYPE found ')
 elif 'type' in content:
-    print('CROSS SITE SCRIPTING ATTACK DETECTED\n type found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n type found ')
 elif 'cat' in content:
-    print('CROSS SITE SCRIPTING ATTACK DETECTED\n cat found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n cat found ')
 elif 'cp' in content:
-    print('CROSS SITE SCRIPTING ATTACK DETECTED\n cp found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n cp found ')
 elif 'ls' in content:
-    print('CROSS SITE SCRIPTING ATTACK DETECTED\n ls found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n ls found ')
 elif 'dir' in content:
-    print('CROSS SITE SCRIPTING ATTACK DETECTED\n dir found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n dir found ')
 elif 'echo' in content:
-    print('CROSS SITE SCRIPTING ATTACK DETECTED\n echo found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n echo found ')
 elif '../' in content:
-    print('local file inclusion and remote file inclusion ATTACK DETECTED\n ../ found in log')
+    print('local file inclusion and remote file inclusion ATTACK DETECTED\n ../ found ')
 elif '/etc/passwd' in content:
-    print('local file inclusion and remote file inclusion ATTACK DETECTED\n ALERT found in log')
+    print('local file inclusion and remote file inclusion ATTACK DETECTED\n /etc/passwd found ')
 elif '/' in content:
-    print('local file inclusion and remote file inclusion ATTACK DETECTED\n ALERT found in log')
+    print('local file inclusion and remote file inclusion ATTACK DETECTED\n / found ')
 elif 'ID' in content:
-    print('COMMAND INJECTION ATTACK DETECTED\n ID found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n ID found ')
 elif 'id' in content:
-    print('COMMAND INJECTION ATTACK DETECTED\n id found in log')
+    print('COMMAND INJECTION ATTACK DETECTED\n id found ')
 elif 'nikto' in content:
-    print('NIKTO DETECTED\n nikto found in log')
+    print('NIKTO DETECTED\n nikto found ')
 else:
-    print('string does not exist')
+    print('No threat Detected')
